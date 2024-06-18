f2c9e827cd28b27c9198bd937f57e2638e67e9c5
Thomas Auinger <thomas@auinger.de>
2024-06-18 14:35:38 +0200

    added 080__nexus

---
+++ 080__nexus/src/main/java/de/codevibe/App80.java
@@ -0,0 +1,10 @@
+package de.codevibe;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class App80 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World " + StringUtils.repeat("!", 10));
+    }
+}
